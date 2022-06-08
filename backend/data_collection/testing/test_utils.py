6e40f50ac6e903f44470f30ff460acb23c845251
Riley Thompson <rileythompson@Rileys-MBP.attlocal.net>
2022-06-07 20:35:32 -0700

    Refactoring through Test Driven Development

--- backend/data_collection/testing/test_utils.py
+++ backend/data_collection/testing/test_utils.py
@@ -2,4 +2,9 @@ from backend.data_collection.utils import Positions
 
 class TestUtils():
     def test_positions_has_value(self):
+        assert Positions.has_value('rb')
+        assert Positions.has_value('qb')
+        assert Positions.has_value('te')
         assert Positions.has_value('wr')
+        assert Positions.has_value('k')
+        assert Positions.has_value('dst')
