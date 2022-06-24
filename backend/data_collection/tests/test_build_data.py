5edb7ea3b48aebf99783422de7602b85896a74bc
Riley Thompson <rjthompson2@dons.usfca.edu>
2022-06-24 08:57:13 -0700

    removed fix_ecr tests

--- backend/data_collection/tests/test_build_data.py
+++ backend/data_collection/tests/test_build_data.py
@@ -1,5 +1,5 @@
 from backend.data_collection.Bootstrap import get_bootstrap, get_cf
-from backend.data_collection.BuildData import build_players, fix_ecr
+from backend.data_collection.BuildData import build_players
 from backend.data_collection.Collectors import Collector, FPTSDataCollector
 from backend.data_collection.Cleaners import ADPCleaner, ECRCleaner, FPTSCleaner
 from backend.data_collection.utils import get_season_year
@@ -54,17 +54,3 @@ class TestBuildData():
 
         replacement_players, replacement_values = build_players(cf_df)
         assert "TE" in list(replacement_values.keys())
-
-    def test_fix_ecr(self):
-        adp = Collector(ws=WebScraper(), url="https://www.fantasypros.com/nfl/adp/ppr-overall.php", _id='id', tag='data')
-        adp_cleaner = ADPCleaner()
-        ecr = Collector(ws=DynamicWebScraper(), url="https://www.fantasypros.com/nfl/rankings/ppr-cheatsheets.php", _id='id', tag='ranking-table')
-        ecr_cleaner = ECRCleaner()
-        ecr_df = ecr.collect_data()
-        adp_df = adp.collect_data()
-
-        ecr_df = ecr_cleaner.clean_data(ecr_df)
-        adp_df = adp_cleaner.clean_data(adp_df)
-
-        ecr_df = fix_ecr(ecr_df, adp_df)
-        assert set(ecr_df.columns) == set(["PLAYER", "ECR"])
