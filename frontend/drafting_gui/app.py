ce47b661fc0c82bafd9cd437da83ccdecf65a179
Riley Thompson <rjthompson2@dons.usfca.edu>
2022-06-28 20:06:49 -0700

    basic frontend flask website

---
+++ frontend/drafting_gui/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+from draft_visualization import draft
+
+app = Flask(__name__)
+app.register_blueprint(draft, url_prefix="/draft")
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
