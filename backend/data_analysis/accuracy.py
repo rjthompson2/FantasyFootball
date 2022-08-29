e3313ae2d55a50589a59c73e5fc155fe6bb90c16
Riley Thompson <rjthompson2@dons.usfca.edu>
2022-08-28 20:01:05 -0700

    improved readability

--- backend/data_analysis/accuracy.py
+++ backend/data_analysis/accuracy.py
@@ -9,13 +9,13 @@ LOG = logging.getLogger(__name__)
 
 def error_calculator(prediction: pd.DataFrame, actual: pd.DataFrame, on:list, keep:list=None):
     diff_df = {}
-    new = pd.merge(actual, prediction, on='PLAYER')
+    merged_dfs = pd.merge(actual, prediction, on='PLAYER')
 
     for column in on:
         #subtract all rows, get the absolute value, sums them, then gets the total, and averages it
-        difference_df = round(abs(new[on+'_x'].sub(new[on+'_y'])), 2)
+        difference_df = round(abs(merged_dfs[on+'_x'].sub(merged_dfs[on+'_y'])), 2)
         #subtract all rows, get the absolute value, divide by original, multiply by 100, then gets the total, and averages it
-        accuracy_df = round(100-abs(new[on+'_x'].sub(new[on+'_y'])).div(new[on+'_x']).mul(100), 2)
+        accuracy_df = round(100-abs(merged_dfs[on+'_x'].sub(merged_dfs[on+'_y'])).div(merged_dfs[on+'_x']).mul(100), 2)
         accuracy_df = accuracy_df.apply(lambda x: 0 if x < 0 or x > 100 else x)
 
         diff_df[on+"Numeric"] = difference_df
@@ -25,9 +25,9 @@ def error_calculator(prediction: pd.DataFrame, actual: pd.DataFrame, on:list, ke
         for item in keep:
             if item not in diff_df:
                 if item == on:
-                    diff_df['Expected'] = new[item+'_y'] #prediction FPTS
-                    diff_df['Actual'] = new[item+'_x'] #actual FPTS
+                    diff_df['Expected'] = merged_dfs[item+'_y'] #prediction FPTS
+                    diff_df['Actual'] = merged_dfs[item+'_x'] #actual FPTS
                 else:
-                    diff_df[item] = new[item]
+                    diff_df[item] = merged_dfs[item]
 
     return diff_df
