c0338742a9408ed297db423b5441d6ba6782cf3b
Riley Thompson <rjthompson2@dons.usfca.edu>
2022-08-28 19:52:24 -0700

    maintenance on CorrelationMap. Also added uniformity to RB correlation map

--- backend/data_analysis/CorrelationMap.py
+++ backend/data_analysis/CorrelationMap.py
@@ -1,4 +1,5 @@
 from datetime import date
+from backend.utils import find_in_data_folder
 import backend.CollectPlayerData
 import pandas as pd
 import seaborn as sn
@@ -19,18 +20,18 @@ def main(start_year, end_year):
 def qb(start_year, end_year):
     df = pd.DataFrame()
     for year in range(start_year, end_year+1):
-        qb_df = pd.read_csv('data/HistoricData/qb_data_'+str(year)+'.csv')
+        qb_df = pd.read_csv(find_in_data_folder('HistoricData/qb_data_'+str(year)+'.csv'))
         qb_df = qb_df.drop('RANK', axis=1)
         df = df.append(qb_df)
     
-    make_map(df, name='data/CorrelationMatrix/qb_matrix.png')
+    make_map(df, name=find_in_data_folder('CorrelationMatrix/qb_matrix.png'))
 
 def rb(start_year, end_year):
     df = pd.DataFrame()
     for year in range(start_year, end_year+1):
-        rb_df = pd.read_csv('data/HistoricData/rb_data_'+str(year)+'.csv')
-        rb_op_df = pd.read_csv('data/HistoricData/rb_opportunity_'+str(year)+'.csv')
-        rb_share_df = pd.read_csv('data/HistoricData/rb_share_'+str(year)+'.csv')
+        rb_df = pd.read_csv(find_in_data_folder('HistoricData/rb_data_'+str(year)+'.csv'))
+        rb_op_df = pd.read_csv(find_in_data_folder('HistoricData/rb_opportunity_'+str(year)+'.csv'))
+        rb_share_df = pd.read_csv(find_in_data_folder('HistoricData/rb_share_'+str(year)+'.csv'))
 
         values = rb_op_df['RA/G']
         rb_df = rb_df.join(values)
@@ -40,14 +41,18 @@ def rb(start_year, end_year):
         rb_df = rb_df.drop('RUSHING 20+', axis=1)
         rb_df = rb_df.drop('RANK', axis=1)
         df = df.append(rb_df)
+    df = df[['PLAYER', 'RUSHING ATT', 'RUSHING YDS', 'RUSHING Y/A', 
+        'RUSHING LG','RUSHING TD', 'REC', 'TGT', 'YDS', 'Y/R', 'TD', 
+        'FL', 'ROST', 'POS', 'IT', 'IT/G', 'RA/G', 'Avg. Rushing Share',
+       'Avg. Rushing Yards Share', 'G', 'FPTS','FPTS/G']]
 
-    make_map(df, name='data/CorrelationMatrix/rb_matrix.png')
+    make_map(df, name=find_in_data_folder('CorrelationMatrix/rb_matrix.png'))
 
 def wr_te(start_year, end_year):
     final_wr_df = pd.DataFrame()
     final_te_df = pd.DataFrame()
     for year in range(start_year, end_year+1):
-        df = pd.read_csv('data/HistoricData/wr-te_data_'+str(year)+'.csv')
+        df = pd.read_csv(find_in_data_folder('HistoricData/wr-te_data_'+str(year)+'.csv'))
         df = df.drop('20+', axis=1)
 
         wr_df = df.loc[df['POS'] == 'WR']
@@ -56,7 +61,7 @@ def wr_te(start_year, end_year):
         te_df = df.loc[df['POS'] == 'TE']
         te_df = get_players(te_df)
 
-        share_df = pd.read_csv('data/HistoricData/wr-te_wopr_'+str(year)+'.csv')
+        share_df = pd.read_csv(find_in_data_folder('HistoricData/wr-te_wopr_'+str(year)+'.csv'))
         share_df['PLAYER'] = share_df['Receiver']
 
         wr_df = merge(wr_df, share_df)
@@ -66,8 +71,8 @@ def wr_te(start_year, end_year):
         final_wr_df = final_wr_df.append(wr_df)
         final_te_df = final_te_df.append(te_df)
     
-    make_map(final_wr_df, name='data/CorrelationMatrix/wr_matrix.png')
-    make_map(final_te_df, name='data/CorrelationMatrix/te_matrix.png')
+    make_map(final_wr_df, name=find_in_data_folder('CorrelationMatrix/wr_matrix.png'))
+    make_map(final_te_df, name=find_in_data_folder('CorrelationMatrix/te_matrix.png'))
 
 def get_players(df):
     df['PLAYER'] = df['PLAYER'].apply(lambda x: re.sub(' \(.+\)', '', x))
