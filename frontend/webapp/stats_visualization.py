cab50266b8b2ec394ed6c7f32aa9233b1290255b
Riley Thompson <rjthompson2@dons.usfca.edu>
2022-12-02 13:10:03 -0800

    moves files from backend to static

--- frontend/webapp/stats_visualization.py
+++ frontend/webapp/stats_visualization.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request, current_app
 from backend.utils import find_in_data_folder
+import shutil
 import os
 
 stats = Blueprint(__name__, "stats")
@@ -15,4 +16,9 @@ def teams():
     file_name = str(request.args.get('id'))
     if file_name == 'None':
         file_name = "schedule_remaining.png"
+    move_file('EPA/'+file_name)
     return render_template("epa.html", file_name=file_name)
+
+def move_file(file_name, destination='frontend/webapp/static'):
+    src = find_in_data_folder(file_name)
+    shutil.copy(src, destination)
