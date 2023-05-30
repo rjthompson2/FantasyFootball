367a3c36d6a80a1e6420f143efa7dad7d0dfccfc
Riley Thompson <rjthompson2@dons.usfca.edu>
2023-05-30 12:58:29 -0700

    database

---
+++ backend/database/database_commands.py
@@ -0,0 +1,25 @@
+import psycopg2
+from backend.database import database
+
+
+def add(tablename, values_dict):
+    cursor, connection = database.connect()
+    command = """INSERT INTO {tablename}("""
+    keys = values_dict.keys
+    for i in len(keys):
+        command += keys[i]
+        if i != len(keys)-1:
+            command +=","
+    command += """)  VALUES("""
+    values = values_dict.values
+    for i in len(values):
+        command += values[i]
+        if i != len(values)-1:
+            command +=","
+    command += ");"
+
+    cursor.execute(command)
+
+    connection.commit()
+
+    database.close(cursor, connection)
